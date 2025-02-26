15ac8dba47a2728e0c8a8ffd952823a3974fbae3
Muhammad <meakub86@gmail.com>
2025-02-25 21:39:34 -0500

    test lambda function 1

--- aws-lambda-project/src/lambda_function_1/handler.py
+++ aws-lambda-project/src/lambda_function_1/handler.py
@@ -1,8 +1,25 @@
+import requests
 from common.utils import log_message
 
 def lambda_handler(event, context):
     log_message('Lambda Function 1 is processing the event.')
+    BASE_URL = "https://jsonplaceholder.typicode.com/posts"
+
+    post_data = {
+        "title": "My First Post",
+        "body": "This is a simple post",
+        "userId": 1
+    }
+
+    response = requests.post(BASE_URL, data=post_data)
+    if(response.status_code == 201):
+        return {
+            'statusCode': 201,
+            'body': 'post created'
+        }
+
+
     return {
-        'statusCode': 200,
-        'body': 'Function 1 processed the event!'
+        'statusCode': response.status_code,
+        'body': 'post failed'
     }
--- aws-lambda-project/src/lambda_function_1/test_lambda_function_1.py
+++ aws-lambda-project/src/lambda_function_1/test_lambda_function_1.py
@@ -1,5 +1,7 @@
 import json
 from lambda_function_1.handler import lambda_handler
+import pytest
+import request_mock
 
 def test_lambda_handler():
     # Sample event and context
@@ -10,5 +12,22 @@ def test_lambda_handler():
     response = lambda_handler(event, context)
 
     # Assert the status code and body in the response
-    assert response['statusCode'] == 200
-    assert response['body'] == "Function 1 processed the event!"
+    assert response['statusCode'] == 201
+    assert response['body'] == "post created"
+
+@pytest.fixture
+def event():
+    '''Mock event'''
+    return {}
+
+@pytest.fixture
+def context():
+    '''mock context'''
+    return {}
+
+def test_lambda_handler_failue(requests_mock, event, context):
+    '''test failed with 500 internal server error'''
+    requests_mock.post("https://jsonplaceholder.typicode.com/posts", statue_code=500)
+    response = lambda_handler(event, context)
+    assert response["statusCode"] == 500
+    assert response["boday"] == "post failed"
